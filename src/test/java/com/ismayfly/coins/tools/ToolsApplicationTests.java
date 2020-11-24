9aec873aa3187cc0a3d759ceafe99db43017fd64
jinlu <jinlu@edspay.com>
2020-11-24 15:02:15 +0800

    Initial commit

---
+++ src/test/java/com/ismayfly/coins/tools/ToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismayfly.coins.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
